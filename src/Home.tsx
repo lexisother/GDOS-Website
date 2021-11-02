ddf2f6b00ed08c370e534188c24e1b78dcfaf7a1
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-02 21:47:43 +0100

    [Home] Add the crappy video

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import {graphql} from "gatsby";
 import {StaticImage} from "gatsby-plugin-image";
 import {Trans} from "gatsby-plugin-react-i18next";
+import Modal from "react-modal";
 import Page from "./shared/Page";
 
 export const query = graphql`
@@ -19,8 +20,21 @@ export const query = graphql`
 `;
 
 export default function HomePage(): JSX.Element {
+    // Set up hooks for the ModalVideo.
+    const [isOpen, setOpen] = useState(false);
+
+    const toggleModal = () => {
+        setOpen(!isOpen);
+    };
+
     return (
         <Page>
+            <Modal isOpen={isOpen} onRequestClose={toggleModal} contentLabel="video">
+                <video width="320" height="240" controls>
+                    <source src="https://nova-vps.ml/~alyxia/static/bedrijf.webm" type="video/webm" />
+                </video>
+            </Modal>
+
             <figure className="avatar">
                 <StaticImage
                     className="logo"
@@ -35,6 +49,23 @@ export default function HomePage(): JSX.Element {
             {/* TODO: Write a short pitch */}
             <div className="mainContent">
                 <Trans>This is the home-page of Gilde DevOps Solutions.</Trans>
+                <div
+                    style={{
+                        marginTop: "3rem",
+                        marginRight: "15rem",
+                        marginLeft: "15rem",
+                        borderRadius: "10px",
+                        backgroundColor: "cornflowerblue",
+                        cursor: "pointer"
+                    }}
+                    onClick={toggleModal}
+                >
+                    <b>
+                        <em>
+                            <Trans>Introductory Video</Trans>
+                        </em>
+                    </b>
+                </div>
             </div>
         </Page>
     );
